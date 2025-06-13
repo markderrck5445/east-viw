614705ab0580c2aa4aae87234b212f03b478db69
markderrck5445 <nguyagwamarkderrick@gmail.com>
2025-06-13 20:13:20 +0300

    added new files and about page and changed  the about page to another page

--- east-viw/src/pages/Home.js
+++ east-viw/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import pic1 from './img/pic1.jpg';
 
 function Home() {
   return (
     <>
       <div className="home-container">
-          <h1>Welcome to East View</h1>
+          <h1>Welcome to East View<span>Trainig Institute</span></h1>
           <p>Enrollment at East View is designed to be a seamless and supportive experience for every prospective student. We welcome individuals from diverse backgrounds who are eager to pursue excellence and personal growth.
           </p>
       </div>
@@ -18,6 +19,18 @@ function Home() {
             <p>
               East View is dedicated to providing a transformative educational experience that empowers students to excel academically and personally. Our commitment to excellence, integrity, and community shapes everything we do.
               </p>
+          </div>
+        </div>
+        
+        <div className="home-section-hero">
+          <div className="hero-image">
+            <img src={pic1} alt="Hero" />
+            </div>
+          <div className="hero-text">
+            <h2>Become a great leader in just a few weeks</h2>
+            <p>
+              Join our culinary program and learn from experienced chefs. Our hands-on approach ensures you gain practical skills and knowledge to excel in the culinary arts.
+          </p>
           </div>
         </div>
     </>
